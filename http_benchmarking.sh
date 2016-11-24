7bdfc611de1ba72655f15db75682c05b9067d4cc
cayetanobv <cayetano.benavent@geographica.gs>
2016-11-25 00:16:03 +0100

    license header to bash files

--- http_benchmarking.sh
+++ http_benchmarking.sh
@@ -9,6 +9,25 @@ _BTHREADS=1
 _BTIME=5
 _BHOST="http://127.0.0.1:8090/"
 _BJSON={"Name":"HelloWorld","Types":["hello","world"]}
+#
+#  Author: Cayetano Benavent, 2016.
+#  cayetano.benavent@geographica.gs
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
 
 _PYTHON_SANIC="python3 python/sanic_simplehttpserver.py"
 _GOLANG_STD="./golang/simple_http_server_json"
